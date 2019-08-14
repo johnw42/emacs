772372f8c67b254fff40906c59240431f7e3b373
John Williams <jrw@pobox.com>
2019-08-13 18:57:49 -0700

    removed commented-out code

--- src/scheme_lisp.c
+++ src/scheme_lisp.c
@@ -14,23 +14,14 @@
 
 static bool scheme_initialized = false;
 
-/* static EMACS_INT next_lisp_object_id = 0; */
-/* static Lisp_Object lisp_object_to_id_table; */
-/* static Lisp_Object lisp_id_to_object_table; */
-
 static ptr lisp_to_scheme(Lisp_Object lisp_obj) {
-  /* CALLN (Fmessage, build_string("lisp_to_scheme: %S"), lisp_obj); */
-  //printf("lisp_to_scheme: %"PRIdPTR"\n", (intptr_t)XLI(lisp_obj));  
-  
   if (SYMBOLP(lisp_obj)) {
     Lisp_Object name = SYMBOL_NAME(lisp_obj);
     ptrdiff_t len = SBYTES(name);
     char *buffer = alloca(1 + len);
     memcpy(buffer, SSDATA(name), len);
     buffer[len] = '\0';
-    /* printf("converting symbol: %s\n", buffer); */
     ptr p = Sstring_to_symbol(buffer);
-    /* printf("done\n"); */
     return p;
   }
 
@@ -61,18 +52,6 @@ static ptr lisp_to_scheme(Lisp_Object lisp_obj) {
 
   /* Make sure this Lisp object has an ID number and wrap it as a
      Scheme object. */
-  /* EMACS_INT obj_id; */
-  /* Lisp_Object lisp_id = Fgethash(lisp_obj, lisp_object_to_id_table, Qnil); */
-  /* if (NILP(lisp_id)) { */
-  /*   obj_id = next_lisp_object_id++; */
-  /*   printf("assigning ID: %d %d\n", (int)obj_id, (int)NILP(lisp_obj)); */
-  /*   Lisp_Object lisp_id = make_number(obj_id); */
-  /*   Fputhash(lisp_obj, lisp_id, lisp_object_to_id_table); */
-  /*   Fputhash(lisp_id, lisp_id, lisp_id_to_object_table); */
-  /* } else { */
-  /*   eassert(INTEGERP(lisp_id)); */
-  /*   obj_id = XINT(lisp_id); */
-  /* } */
   Lisp_Object obj_id = call1(Qensure_lisp_object_id, lisp_obj);
   EMACS_UINT lisp_obj_uint = (EMACS_UINT) XLI(lisp_obj);
   uptr hi = lisp_obj_uint >> SHIFT_BITS;
@@ -84,8 +63,6 @@ static ptr lisp_to_scheme(Lisp_Object lisp_obj) {
 }
 
 static Lisp_Object scheme_to_lisp(ptr scheme_obj) {
-  /* printf("scheme_to_lisp\n"); */
-
   ptr obj_value;
   Lisp_Object lisp_obj = Qnil;
   
@@ -113,13 +90,9 @@ static Lisp_Object scheme_to_lisp(ptr scheme_obj) {
 
   // If scheme_obj is a lisp-object-ref, extract its value and convert
   // to to a real Lisp_Object reference.
-  /* printf("calling extract-lisp-object from C\n"); */
-  /* eassert(Ssymbolp(Ssymbol_to_string("extract-lisp-object"))); */
-  /* eassert(Sprocedurep(Stop_level_value(Ssymbol_to_string("extract-lisp-object")))); */
   obj_value = Scall1(Stop_level_value(Sstring_to_symbol("extract-lisp-object")),
                      scheme_obj);
   if (obj_value != Sfalse) {
-    /* printf("found object\n"); */
     eassert(Spairp(obj_value));
     ptr hi_obj = Scar(obj_value);
     ptr lo_obj = Scdr(obj_value);
@@ -132,7 +105,6 @@ static Lisp_Object scheme_to_lisp(ptr scheme_obj) {
     goto end;
   }
 
-  /* printf("calling ensure-scheme-object from C\n"); */
   obj_value = Scall1(Stop_level_value(Sstring_to_symbol("ensure-scheme-object-id")),
                      scheme_obj);
   eassert(Sfixnump(obj_value));
@@ -146,8 +118,6 @@ DEFUN ("scheme-funcall", Fscheme_funcall, Sscheme_funcall, 1, MANY, 0,
        doc: /* TODO */)
   (ptrdiff_t nargs, Lisp_Object *args)
 {
-  /* printf("Fscheme_funcall\n"); */
-
   ptr *scheme_args = alloca(nargs * sizeof(ptr));
   for (iptr i = 0; i < nargs; i++) {
     scheme_args[i] = lisp_to_scheme(args[i]);
@@ -163,7 +133,6 @@ DEFUN ("scheme-funcall", Fscheme_funcall, Sscheme_funcall, 1, MANY, 0,
     Sput_arg(i, scheme_args[i - 1]);
   }
   ptr scheme_result = Scall(scheme_args[0], nargs - 1);
-  /* printf("converting result\n"); */
   return scheme_to_lisp(scheme_result);
 }
 
@@ -171,7 +140,6 @@ DEFUN ("scheme-top-level-value", Fscheme_top_level_value, Sscheme_top_level_valu
        doc: /* TODO */)
   (Lisp_Object symbol)
 {
-  /* printf("Fscheme_top_level_value\n"); */
   CHECK_SYMBOL(symbol);
   ptr scheme_symbol = lisp_to_scheme(symbol);
   eassert(Ssymbolp(scheme_symbol));
@@ -182,7 +150,6 @@ DEFUN ("forget-scheme-object", Fforget_scheme_object, Sforget_scheme_object, 1, 
        doc: /* TODO */)
   (Lisp_Object id)
 {
-  /* printf("Fforget_scheme_object\n"); */
   CHECK_NUMBER(id);
   if (scheme_initialized) {
     Scall1(Stop_level_value(Sstring_to_symbol("forget-scheme-object")),
@@ -215,9 +182,7 @@ static ptr scheme_elisp_apply(ptr func, ptr args) {
     nargs++;
   }
   Lisp_Object *lisp_args = alloca(sizeof(Lisp_Object) * nargs);
-  /* printf("elisp-apply: converting function\n"); */
   lisp_args[0] = scheme_to_lisp(func);
-  /* printf("elisp-apply: %d\n", (int)nargs); */
   uptr i = 1;
   for (ptr link = args; link != Snil; link = Scdr(link)) {
     lisp_args[i] = scheme_to_lisp(Scar(link));
@@ -297,59 +262,6 @@ void scheme_gc(void) {
   Sunlock_object(vec);
 }
 
-/* static ptr locked_scheme_objects = Sfalse; */
-/* static iptr num_locked_scheme_objects = 0; */
-
-/* void scheme_lock_for_elisp(ptr x) { */
-/*   eassert(scheme_initialized); */
-/*   Slock_object(x); */
-/*   iptr len = Svector_length(locked_scheme_objects); */
-/*   eassert(len > 0); */
-/*   if (num_locked_scheme_objects == len) { */
-/*     iptr new_len = 2 * len; */
-/*     ptr new_vec = Smake_vector(new_len, Sfalse); */
-/*     Slock_object(new_vec); */
-/*     for (iptr i = 0; i < len; i++) { */
-/*       Svector_set(new_vec, i, Svector_ref(locked_scheme_objects, i)); */
-/*     } */
-/*     Sunlock_object(locked_scheme_objects); */
-/*     locked_scheme_objects = new_vec; */
-/*     len = new_len; */
-/*   } */
-/*   eassert(Svector_length(locked_scheme_objects) > num_locked_scheme_objects); */
-/*   Svector_set(locked_scheme_objects, num_locked_scheme_objects, x); */
-/*   ++num_locked_scheme_objects; */
-/* } */
-
-/* void scheme_unlock_for_elisp(ptr x) { */
-/*   for (iptr i = 0; i < num_locked_scheme_objects; i++) { */
-/*     if (Svector_ref(locked_scheme_objects, i) == x) { */
-/*       --num_locked_scheme_objects; */
-/*       if (num_locked_scheme_objects > 0) { */
-/*         Svector_set(locked_scheme_objects, i, */
-/*                     Svector_ref(locked_scheme_objects, num_locked_scheme_objects)); */
-/*       } */
-/*       Svector_set(locked_scheme_objects, num_locked_scheme_objects, Sfalse); */
-/*       Sunlock_object(x); */
-/*       return; */
-/*     } */
-/*   } */
-/*   eassert(false); */
-/* } */
-
-/* void scheme_gc(void) { */
-/*   if (!scheme_initialized) { */
-/*     return; */
-/*   } */
-/*   for (iptr i = 0; i < num_locked_scheme_objects; i++) { */
-/*     Sunlock_object(Svector_ref(locked_scheme_objects, i)); */
-/*   } */
-/*   Scall0(Stop_level_value(Sstring_to_symbol("collect"))); */
-/*   for (iptr i = 0; i < num_locked_scheme_objects; i++) { */
-/*     Slock_object(Svector_ref(locked_scheme_objects, i)); */
-/*   } */
-/* } */
-
 void syms_of_scheme_lisp(void) {
   DEFSYM(Qscheme_value_ref_id, "scheme-value-ref-id");
   DEFSYM(Qensure_scheme_value_ref, "ensure-scheme-value-ref");
@@ -376,34 +288,9 @@ void scheme_init(void) {
 
   eassert(!scheme_initialized);
 
-  /* lisp_object_to_id_table = */
-  /*   make_hash_table(hashtest_eq, */
-  /*                   DEFAULT_HASH_SIZE, */
-  /*                   DEFAULT_REHASH_SIZE, */
-  /*                   DEFAULT_REHASH_THRESHOLD, */
-  /*                   Qnil, false); */
-  /* lisp_id_to_object_table = */
-  /*   make_hash_table(hashtest_eql, */
-  /*                   DEFAULT_HASH_SIZE, */
-  /*                   DEFAULT_REHASH_SIZE, */
-  /*                   DEFAULT_REHASH_THRESHOLD, */
-  /*                   Qnil, false); */
-  /* lisp_finalizer_table = */
-  /*   make_hash_table(hashtest_eql, */
-  /*                   DEFAULT_HASH_SIZE, */
-  /*                   DEFAULT_REHASH_SIZE, */
-  /*                   DEFAULT_REHASH_THRESHOLD, */
-  /*                   Qnil, false); */
-
-  /* printf("Starting scheme.\n"); */
   Sscheme_init(NULL);
   Sregister_boot_file("/usr/local/google/home/jrw/.local/lib/csv9.5.2/a6le/scheme.boot");
-  /* printf("building heap\n"); */
   Sbuild_heap(NULL, NULL);
-  
-  /* num_locked_scheme_objects = 0; */
-  /* locked_scheme_objects = Smake_vector(1, Sfalse); */
-  /* Slock_object(locked_scheme_objects); */
   
   Sforeign_symbol("scheme_elisp_boundp", scheme_elisp_boundp);
   Sforeign_symbol("scheme_elisp_fboundp", scheme_elisp_fboundp);
